1214430875de60d13cbcf5b8c185f6f77798a9f1
Martín <magadiflo@gmail.com>
2022-07-08 12:46:24 -0500

    Forzar error en tiempo de ejecución para ver el comportamiento en Angular (se comentó dicho código)

--- src/main/java/com/magadiflo/pagination/resource/UserResource.java
+++ src/main/java/com/magadiflo/pagination/resource/UserResource.java
@@ -39,6 +39,9 @@ public class UserResource {
         //Esto se debe eliminar, solo es para probar la interfaz, lo dejaré mientras seguimos desarrollando
         TimeUnit.SECONDS.sleep(1);
 
+        //Forzamos un error en tiempo de ejecución para ver el comportamiento en Angular
+        //throw new RuntimeException("Excepción forzada para la prueba");
+
         Page<User> userPage = this.userService.getUsers(name.orElse(""), page.orElse(0), size.orElse(10));
         Map<String, Page<User>> pageMap = new HashMap<>();
         pageMap.put("page", userPage);
